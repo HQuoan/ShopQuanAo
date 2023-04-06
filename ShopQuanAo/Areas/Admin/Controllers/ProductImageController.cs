503d2f616e7ed8d73ea6d69adc9376b41ba53f7f
HQuoan <huyvodtan@gmail.com>
2023-04-06 18:59:27 +0700

    xong phần ảnh sp

--- ShopQuanAo/Areas/Admin/Controllers/ProductImageController.cs
+++ ShopQuanAo/Areas/Admin/Controllers/ProductImageController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShopQuanAo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,35 @@ namespace ShopQuanAo.Areas.Admin.Controllers
     public class ProductImageController : Controller
     {
         // GET: Admin/ProductImage
+        private ShopQuanAoEntities db = new ShopQuanAoEntities();
         public ActionResult Index(int? id)
         {
-            ViewBag.Id = id;
-            return View();
+            ViewBag.ProductID = id;
+            var ds = db.ProductImages.Where(m => m.ProductID == id).ToList();
+            return View(ds);
+        }
+        [HttpPost]
+        public ActionResult AddImage(int productID,string url)
+        {
+            db.ProductImages.Add(new ProductImage
+            {
+                ProductID = productID,
+                isDefault = false,
+                Image = url
+            });
+            db.SaveChanges();
+            return Json(new { success = true });
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var del = db.ProductImages.Find(id);
+            db.ProductImages.Remove(del);
+            db.SaveChanges();
+            return Json(new { success = true});
         }
     }
 }
